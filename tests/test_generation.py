2c3ff9a3ba89e4d211e959a7a6f8417f502b61b5
nils <kappeler.nils@gmail.com>
2025-07-26 23:22:22 +0200

    fixed generate complete test
    fixed name

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,18 +1,11 @@
 from context import Grid, generators, solvers
-import time
 
 
-def test_generate_solved():
-    start_time = time.perf_counter()
-    count = 1
-    board = Grid.construct_empty().generate_solved()
-    assert board.is_valid()
-    assert len(list(board.iter_positions(0))) == 0
-    assert board.is_complete()
-    duration = time.perf_counter() - start_time
-    print(
-        f"generated {count} solved boards in {duration:.3}s ( {duration / count:.3}s / per borad)"
-    )
+def test_generate_complete():
+    grid = generators.BasicPuzzleGenerator().fill_grid(grid=Grid.construct_empty())
+    assert grid.is_valid()
+    assert len(list(grid.iter_positions(0))) == 0
+    assert grid.is_complete()
 
 
 def test_generate_easy_puzzle():
