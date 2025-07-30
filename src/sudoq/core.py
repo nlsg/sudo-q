d28762ea1e2739c97e17c1e62cf87a7e68bfef0e
nils <kappeler.nils@gmail.com>
2025-07-31 00:43:47 +0200

    removed nonsensical method

--- src/sudoq/core.py
+++ src/sudoq/core.py
@@ -20,6 +20,3 @@ class Cell:
 
     def is_empty(self) -> bool:
         return self.value == 0
-
-    def with_value(self, value: Digit) -> "Cell":
-        return Cell(value=value, candidates=self.candidates)
