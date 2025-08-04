0e4d909c29c79cd8e7b1b060918f1eee68ab2a84
nils <kappeler.nils@gmail.com>
2025-08-05 01:19:27 +0200

    impl. performance check hook

---
+++ .pre-commit-hooks/check-performance.py
@@ -0,0 +1,21 @@
+import timeit
+from sudoq import Grid
+from sudoq.solvers import BacktrackingSolver
+
+
+def check_performance():
+    time = min(
+        timeit.repeat(
+            lambda: BacktrackingSolver().solve(Grid.construct_empty()),
+            number=1,
+            repeat=3,
+        )
+    )
+    if time > 0.5:
+        print(f"Performance regression detected: {time:.2f}s")
+        return 1
+    return 0
+
+
+if __name__ == "__main__":
+    exit(check_performance())
